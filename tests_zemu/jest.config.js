9e1201642308f85ae08e91d645b8350cd8fe3684
Carlos Medeiros <104493454+chcmedeiros@users.noreply.github.com>
2026-04-16 15:59:52 +0100

    Merge pull request #46 from Zondax/improvements
    Improvements: parser hardening and dependency refresh

--- tests_zemu/jest.config.js
+++ tests_zemu/jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transformIgnorePatterns: ['^.+\\.js$'],
+  // @zondax/zemu 0.66+ pulls get-port@7 which ships as pure ESM. Route .js
+  // files through ts-jest (which transpiles `import` to `require` using the
+  // CommonJS tsconfig module setting) and allow get-port to be transformed
+  // instead of being skipped as a node_modules entry.
+  transform: {
+    '^.+\\.[jt]sx?$': ['ts-jest', { isolatedModules: true }],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!get-port/)'],
 }
